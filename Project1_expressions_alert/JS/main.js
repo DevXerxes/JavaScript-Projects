b287b4a955cbffdfbd1a2dbe90c9473f38533115
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2021-08-16 21:39:55 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+var B = "Norma my Queen!" //assigning a value to a variable
+
+window.alert(B); //creates a pop-up window with the value of variable B= Norma my Queen
+
+document.write('Life is full of opportunities, all you have to do is PAY ATTENTION.'); //the way to display text in website and display values of variables
+
+var A = "I am Xerxes.";
+
+document.write(A);
+
+document.write("\"I\'ll get you next time rabbit!\"" //here we concatenated a text, the how -to for displaying quotations in text on browser
++ "The turtle uttered in disappointment.");
+
+var C = "Then the rabbit looked over his shoulder with a quirky smile and said," //the + is concatenating= adding text to recetn text input
++ "\"Only if you can catch me!\""
+
+document.write(C);
+
+var FastFurious = "Cars", Dom = "Camaro", John = "Subaru", Letty = "GTR", Me = "Camry";
+var FastFurious = FastFurious.fontcolor("Red");
+var Dom = Dom.fontcolor("Blue");
+var John = John.fontcolor("Yellow");
+var Letty = Letty.fontcolor("purple");
+var Me = Me.fontcolor("green"); //here we assigned a color to each value of each variable
+document.write(Me);
+document.write(Letty);
+
+document.write(14 + 7); //wrote an an expression using a statement
